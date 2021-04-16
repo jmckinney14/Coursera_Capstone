27b6eec6e516f5ab8a414288a7c7e3830d81f198
Joe McKinney <66033829+jmckinney14@users.noreply.github.com>
2021-04-16 10:26:05 -0400

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # this will be used for my capstone project
+
+# In[8]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
